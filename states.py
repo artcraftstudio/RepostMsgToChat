56a5992039cb676031179fd22d9a51555027252a
artcraftstudio <143928652+artcraftstudio@users.noreply.github.com>
2023-12-23 16:44:18 +0100

    Delete states.py

--- states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class RepostDataToChat(StatesGroup):
-    input_text = State()
-    confirm_text = State()
-    input_media = State()
-    confirm_media = State()
